fd5fefd0fba3ac4a886d6665beb4164f1ffb3d09
adnan-jahbli <adnan.jahbli@gmail.com>
2023-07-28 18:47:17 +0000

    creating file 102

---
+++ 0x05-processes_and_signals/102-zombie.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+/**
+ * create_zombie_processes - function that creates multiple zombie processes.
+ *
+ * Return: 0
+ */
+int create_zombie_processes(void)
+{
+	while (1)
+	{
+		int i;
+
+		for (i = 0; i < 5; i++)
+		{
+			if (fork() == 0)
+			{
+				printf("Zombie process created,
+						PID: %d\n", getpid());
+				return (0);
+			}
+		}
+	}
+	return (0);
+}
+
+/**
+ * infinite_loop - function that contains an infinite loop.
+ *
+ * Return: 0
+ */
+int infinite_loop(void)
+{
+	while (1)
+	{
+		sleep(1);
+	}
+	return (0);
+}
+
+/**
+ * main - Entry point of the program.
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	create_zombie_processes();
+	infinite_loop();
+	return (0);
+}
